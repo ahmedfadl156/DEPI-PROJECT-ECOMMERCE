a0ed1eb66d70de0944b42f2b3b35bbea9cab2704
ahmedfadl156 <151857143+ahmedfadl156@users.noreply.github.com>
2025-03-24 03:16:39 +0200

    Ecommerce-Website

---
+++ src/app/components/arrivals/arrivals.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-arrivals',
+  imports: [],
+  templateUrl: './arrivals.component.html',
+  styleUrl: './arrivals.component.scss'
+})
+export class ArrivalsComponent {
+
+}
---
+++ src/app/components/topsellers/topsellers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topsellers',
+  imports: [],
+  templateUrl: './topsellers.component.html',
+  styleUrl: './topsellers.component.scss'
+})
+export class TopsellersComponent {
+
+}
